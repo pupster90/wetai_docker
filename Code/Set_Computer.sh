bf0ecb42a9cf2d4efd682632a0bca7de6993afd8
Matthew Elliott <m3elliot@eng.ucsd.edu>
2020-10-20 20:30:01 -0700

    Update Set_Computer.sh

--- Code/Set_Computer.sh
+++ Code/Set_Computer.sh
@@ -1,6 +1,6 @@
 VERSION_NAME=v1.0.0
 # Set io's Version name in custom.js file
-sed -i "s/{VERSION_NAME}/$VERSION_NAME/g"  ~/io_docker_setup/custom.js
+sed -i "s/{VERSION_NAME}/$VERSION_NAME/g"  ~/wetai_io_docker/custom.js
 
 ####################
 ### Basic Setup ###
