6d22372675b58a8897c623c5268279f4039d90fe
Matthew Elliott <m3elliot@eng.ucsd.edu>
2020-10-21 10:00:31 -0700

    Update Set_Computer.sh

--- Code/Set_Computer.sh
+++ Code/Set_Computer.sh
@@ -22,6 +22,14 @@ apt-get install update
 conda update -y conda
 
 
+### Install Docker
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+dockerd &
+
 ### Install nb-extensiosn and GenePattern notebook
 conda install -y -c conda-forge  rise
 conda install -y -c conda-forge jupyter_contrib_nbextensions
