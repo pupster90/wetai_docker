443affad46cd07bd510c9839d9772cceed86be6e
Matthew Elliott <m3elliot@eng.ucsd.edu>
2020-10-28 05:36:34 -0700

    Update Set_Computer.sh

--- Code/Set_Computer.sh
+++ Code/Set_Computer.sh
@@ -77,7 +77,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io
-dockerd &
+#dockerd & #commented because command doesn't work here
 
 ### Install SpikeSort
 
