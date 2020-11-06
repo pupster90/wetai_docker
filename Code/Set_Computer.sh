ce4d4de004060819a49c4db29aaec926e294f881
Matthew Elliott <m3elliot@eng.ucsd.edu>
2020-11-05 16:19:28 -0800

    Update Set_Computer.sh

--- Code/Set_Computer.sh
+++ Code/Set_Computer.sh
@@ -110,4 +110,4 @@ pip install spikeinterface ml_ms4alg boto3
 apt-get -y update
 apt-get -y install vim
 pip install --upgrade kachery
-
+pip install git+https://github.com/flatironinstitute/spikeforest2.git
